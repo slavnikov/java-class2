1895d97ab7ee1a12ec478eade9af9b2c1be276fe
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-10 11:10:41 -0500

    Implement print years and days from minutes method.

--- src/method_overloading/MethodOverloadingExercises.java
+++ src/method_overloading/MethodOverloadingExercises.java
@@ -34,5 +34,15 @@ public class MethodOverloadingExercises {
     }
     
 //==============================================================================
+    
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) { 
+            System.out.println("Invalid Value"); 
+            return; 
+        }
+        
+        long days = minutes / 60 / 24;
+        System.out.println(minutes + " min = " + (days / 365) + " y and " + (days % 365) + " d");
+    }
 }
 
