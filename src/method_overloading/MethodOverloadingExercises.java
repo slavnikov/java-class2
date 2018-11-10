4ddfdb87c123a6d5cab21e0ae0c5cb77a1c98f13
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-10 10:36:41 -0500

    Add a leading zeroes method.'

--- src/method_overloading/MethodOverloadingExercises.java
+++ src/method_overloading/MethodOverloadingExercises.java
@@ -3,12 +3,22 @@ package method_overloading;
 public class MethodOverloadingExercises {
     public static String getDurationString(int min, int sec) {
         if ((min < 0) || (sec < 0 || sec > 59)) { return "Invalid Input"; }
-        return (min / 60) + "h " + (min % 60) + "m " + sec + "s";
+        return leadOs(min / 60) + "h " + 
+                leadOs(min % 60) + "m " + 
+                leadOs(sec) + "s";
     }
     
     public static String getDurationString(int sec) {
         if (sec < 0) { return "Invalid Input"; }
         return getDurationString(sec / 60, sec % 60);
+    }
+    
+    public static String leadOs(int num) {
+        if (num > 9) {
+            return "" + num;
+        } else {
+            return "0" + num;
+        }
     }
     
 //==============================================================================
