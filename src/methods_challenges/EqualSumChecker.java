c1a2170e5c875b5beaf59da486ab9ad16676d276
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-10 09:51:50 -0500

    Implement an equal sum checker.

---
+++ src/methods_challenges/EqualSumChecker.java
@@ -0,0 +1,7 @@
+package methods_challenges;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int first, int second, int sum) {
+        return first + second == sum;
+    }
+}
