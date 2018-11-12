8a381ce23279fdf966957b86b86ee5fba635cd49
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-12 15:33:40 -0500

    Update decimal comparator.

--- src/methods_challenges/DecimalComparator.java
+++ src/methods_challenges/DecimalComparator.java
@@ -1,6 +1,11 @@
 package methods_challenges;
 
 public class DecimalComparator {
+    
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(3.745, 3.746));
+    }
+    
     public static boolean areEqualByThreeDecimalPlaces(double first, double second) {
         double difference = first * 1000 - second * 1000;
         return difference < 1 && difference > -1;
