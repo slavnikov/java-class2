7537f407916407ce1ffe49ba7f361c810e013952
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-11-13 11:09:26 -0500

    Start implementation of loop methods.

---
+++ src/loops/Loops.java
@@ -0,0 +1,44 @@
+package loops;
+
+public class Loops {
+    
+    public static void main(String[] args) {
+        fizzBuzzLoop();
+    }
+    
+    public static void fizzBuzzLoop()  {
+        int sum = 0;
+        int finds = 0;
+        
+        
+        for (int i = 1; i <= 1000; i++){
+            if(i % 5 == 0 && i % 3 == 0) {
+                sum += i;
+                finds++;
+            }
+            if (finds >= 5) {break;}
+        }
+        
+        System.out.println(sum);
+    }
+    
+//==============================================================================
+    
+    public static boolean isOdd(int input) {
+        return input > 0 && input % 2 == 1;
+    }
+    
+    public static int sumOdd(int start, int end) {
+        if(start <= 0 || end <= 0 || start > end) { return -1;}
+        
+        int sum = 0;
+        
+        for (int i = start; i <= end; i++) {
+            if(isOdd(i)) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+}
